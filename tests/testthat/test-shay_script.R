b7432c181cd87a0552e0937d07c4f6874e1ac979
mattangoh <mattangoh@gmail.com>
2024-02-05 18:32:18 -0800

    updated testing yml

---
+++ tests/testthat/test-shay_script.R
@@ -0,0 +1,21 @@
+install.packages("testthat")
+library(testthat)
+
+source("../../R/shay_script.R")
+
+# Test Cases
+test_that("GetBiggestVenues returns correct ggplot object", {
+  result <- GetBiggestVenues("England", 5, "mock_api_key")
+  expect_is(result, "ggplot")
+})
+
+test_that("GetCoachInfo returns correct data frame", {
+  result <- GetCoachInfo("team_id_example", "mock_api_key")
+  expect_is(result, "data.frame")
+  expect_equal(ncol(result), 8) # Check for 8 columns
+})
+
+test_that("CompareTrophies returns correct ggplot object", {
+  result <- CompareTrophies("player1_id", "player2_id", "mock_api_key")
+  expect_is(result, "ggplot")
+})
